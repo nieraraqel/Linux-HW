4ab5635691e94b04fa80564d1a716161e1e0ce01
niera <71599424+nieraraqel@users.noreply.github.com>
2024-10-17 00:05:40 +0400

    Update main.cpp

--- FS-1/main.cpp
+++ FS-1/main.cpp
@@ -1,110 +1,47 @@
-#include <iostream>
+#include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <cerrno>
-#include <cstring>
-#include <sstream>
-#include <cmath>
-#include <limits>
+#include <errno.h>
+#include <stdlib.h>
 
-#define BUFFER_SIZE 4096
+#define BUFFER_SIZE 4097
 
-int main(int argc, char** argv){
+void cat(char *name) {
+  int file = open(name, O_RDONLY);
 
-  if(argc != 3){
-    std::cerr << "Missing filename arguments or there are more than 2 arguments " << std::endl;
-    exit(1);
-  }
-
-  const char* source_filename = argv[1];
-  const char* destination_filename = argv[2];
-
-  int sourceFd = open(source_filename, O_RDONLY);
-
-  if(sourceFd == -1){
-    std::cerr << "Error while opening the file " << source_filename << ". " << strerror(errno) << std::endl;
+  if(file == -1){
+    printf("Error\n");
     exit(errno);
   }
-
-  int destinationFd = open(destination_filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP);
-
-  if(destinationFd == -1){
-    std::cerr << "Error while opening the file " << destination_filename << ". " << strerror(errno) << std::endl;
-    exit(errno);
-  }
-
+  char* buffer = (char*)malloc(BUFFER_SIZE);
   long readBytes = 0;
-  int offset = 0;
-
-  off_t totalBytes = 0;
-  off_t holeBytes = 0;
 
   while(true){
-    off_t holeOffset = lseek(sourceFd, offset, SEEK_HOLE);
+    readBytes = read(file, buffer, BUFFER_SIZE);
 
-    if (holeOffset < 0) {
-      if(errno == ENXIO){
-        break;
-      }
-      std::cerr << "Something went wrong. " << strerror(errno) << std::endl;
+    if(readBytes == -1){
+      printf("Error\n");
       exit(errno);
     }
-
-    if (holeOffset > offset) {
-      off_t holeSize = holeOffset - offset;
-
-      if (lseek(destinationFd, offset, SEEK_SET) == -1) {
-        std::cerr << "Something went wrong. " << strerror(errno) << std::endl;
-        exit(errno);
-      }
-      char* buffer = (char*)malloc(holeSize);
-
-      if (write(destinationFd, buffer, holeSize) != holeSize) {
-        std::cerr << "Something went wrong. " << strerror(errno) << std::endl;
-        free(buffer);
-        exit(errno);
-      }
-
-      holeBytes += holeSize;
-
-      free(buffer);
-    }
-
-
-    lseek(sourceFd, holeOffset, SEEK_DATA);
-
-    char* buffer = (char*)malloc(BUFFER_SIZE);
-
-    readBytes = read(sourceFd, buffer, BUFFER_SIZE);
-
-    if(readBytes < 0) {
-      std::cerr << "Something went wrong. " << strerror(errno) << std::endl;
-      exit(errno);
-    }
-
-    if(readBytes == 0) {
+    if(readBytes == 0){
       break;
     }
-
-    if (write(destinationFd, buffer, readBytes) != readBytes) {
-      std::cerr << "Something went wrong. " << strerror(errno) << std::endl;
-      exit(errno);
-    }
-
-    offset = holeOffset + readBytes;
-
-    free(buffer);
+    buffer[readBytes] = '\0';
+    printf("%s", buffer);
   }
 
-  off_t fileSize = lseek(sourceFd, 0, SEEK_END);
-  if (fileSize > totalBytes) {
-    holeBytes += fileSize - totalBytes;
+  free(buffer);
+  close(file);
+}
+
+int main(int argc, char **argv) {
+  if (argc < 2){
+    printf("Error\n");
+    exit(1);
   }
-
-  std::cout << "Successfully copied " << totalBytes << " bytes (data: " << totalBytes - holeBytes << ", hole: " << holeBytes << ")." << std::endl;
-
-  close(sourceFd);
-  close(destinationFd);
+  for (int i = 1; i < argc; ++i){
+    cat(argv[i]);
+  }
 
   return 0;
 }
