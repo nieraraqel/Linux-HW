e4c2d66fb89a69ddb5578ecb8fba7e0c3ef9ff5e
niera <71599424+nieraraqel@users.noreply.github.com>
2025-03-24 15:39:04 +0400

    Create client.cpp

---
+++ rpc-tcp/client.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <poll.h>
+#include <errno.h>
+
+int main() {
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Socket creation error\n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("Invalid address/ Address not supported\n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("Connection Failed\n");
+        return -1;
+    }
+
+    while (1) {
+        printf("Enter message: ");
+        fgets(buffer, 1024, stdin);
+        send(sock, buffer, strlen(buffer), 0);
+        int valread = read(sock, buffer, 1024);
+        buffer[valread] = '\0';
+        printf("Server: %s\n", buffer);
+    }
+
+    close(sock);
+    return 0;
+}
